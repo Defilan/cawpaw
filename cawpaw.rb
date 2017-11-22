4d83d64541678c6f1fb707ab2dc20cd4719af106
Christopher P. Maher <defilan@gmail.com>
2017-11-22 11:48:19 -0800

    Updating some basic code

--- cawpaw.rb
+++ cawpaw.rb
@@ -5,11 +5,15 @@ set :static, true
 set :public_folder, 'static'
 set :views, 'views'
 
-get '/add/' do
+get '/' do
+  'Welcome to CawPaw: A tool for learning basic math'
+end
+
+get '/add' do
   erb :add_form
 end
 
-post '/add/' do
+post '/add' do
   firstnum = params[:firstnum]
   secondnum = params[:secondnum]
   erb :add, locals: { 'firstnum' => firstnum, 'secondnum' => secondnum }
